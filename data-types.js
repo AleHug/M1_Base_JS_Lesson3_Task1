2f3d06dd2246cc31a3006ae16e4b12b52ee36600
AleHug <93007065+AleHug@users.noreply.github.com>
2022-01-12 20:25:48 +0100

    Add files via upload

---
+++ data-types.js
@@ -0,0 +1,35 @@
+// Number//
+const myAge = 30;
+console.log(myAge, 'myAge', typeof myAge);
+
+// String //
+const myName = 'Alex';
+console.log(myName, 'myName', typeof myName);
+
+// Boolean //
+const car = true;
+console.log(car, 'car', typeof car);
+
+// Null //
+const eyecolor = null;
+console.log(eyecolor, 'eyercolor', typeof eyecolor);
+
+// Undefined //
+const athlete = undefined;
+console.log(athlete, 'athlete', typeof athlete );
+
+// Object//
+const person = {
+    name: 'Alex',
+    age: 30,
+    athlete: undefined,
+};
+console.log(person);
+
+// bigint //
+const  bigintNumber = BigInt(10);
+console.log(bigintNumber, 'bigintNumber', typeof bigintNumber);
+
+// Symbol //  
+const id = Symbol('id');
+console.log(id, 'id', typeof id);
